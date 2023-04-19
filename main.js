ab01684fdccab914ed1a65caca0df228f3d34e1f
Denzel Turambi <dturambi26@gmail.com>
2023-04-19 15:09:00 -0600

    Add query selectors and organizational comments

---
+++ main.js
@@ -0,0 +1,7 @@
+// DATA MODEL
+// QUERY SELECTORS
+var classicGame = document.querySelector('#classic-game');
+var difficultGame = document.querySelector('#difficult-game');
+var gameSelection = document.querySelector('.game-selection');
+// EVENT LISTENERS
+// FUNCTIONS/EVENTS
