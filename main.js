aa7a43d88bdd7bfa8254506d2c6f39e16776b0e2
Denzel Turambi <dturambi26@gmail.com>
2023-04-19 21:09:11 -0600

    Create showWeapons function which hides the dashboard

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 // DATA MODEL
 // QUERY SELECTORS
-var classicGame = document.querySelector('#classic-game');
-var difficultGame = document.querySelector('#difficult-game');
+var classicGame = document.querySelector('#classic-game-selection');
+var difficultGame = document.querySelector('#difficult-game-selection');
 var gameSelection = document.querySelector('.game-selection');
+var classicWeaponSelection = document.querySelector('.classic-mode');
+
 // EVENT LISTENERS
+classicGame.addEventListener('click', showWeapons);
+
 // FUNCTIONS/EVENTS
+function showWeapons() {
+  gameSelection.classList.add('hidden');
+  classicWeaponSelection.classList.remove('hidden');
+}
