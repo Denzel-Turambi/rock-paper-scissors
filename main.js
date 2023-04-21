c8694fc4d86bce60ae35976ef2389d2ffd240030
Denzel Turambi <dturambi26@gmail.com>
2023-04-21 13:33:16 -0600

    Create showClassicWeapons and showDifficultWeapons function, could use refactoring but works for now

--- main.js
+++ main.js
@@ -4,13 +4,25 @@ var classicGame = document.querySelector('#classic-game-selection');
 var difficultGame = document.querySelector('#difficult-game-selection');
 var gameSelection = document.querySelector('.game-selection');
 var classicWeaponSelection = document.querySelector('.classic-mode');
+var difficultWeaponSelection = document.querySelector('.difficult-mode');
+var gameSubtitle = document.querySelector('.game-subtitle');
+var weaponSubtitle = document.querySelector('.weapon-subtitle');
 
 // EVENT LISTENERS
-classicGame.addEventListener('click', showWeapons);
-difficultGame.addEventListener('click', showWeapons);
+classicGame.addEventListener('click', showClassicWeapons);
+difficultGame.addEventListener('click', showDifficultWeapons);
 
 // FUNCTIONS/EVENTS
-function showWeapons() {
+function showClassicWeapons() {
   gameSelection.classList.add('hidden');
+  gameSubtitle.classList.add('hidden');
   classicWeaponSelection.classList.remove('hidden');
+  weaponSubtitle.classList.remove('hidden');
+}
+
+function showDifficultWeapons() {
+  gameSelection.classList.add('hidden');
+  gameSubtitle.classList.add('hidden');
+  difficultWeaponSelection.classList.remove('hidden');
+  weaponSubtitle.classList.remove('hidden');
 }
